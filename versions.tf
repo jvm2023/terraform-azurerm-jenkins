c0c283505928c436b9cd18780827f0fdbc35bacd
jvm2023 <cloudscenerios@gmail.com>
2023-07-18 01:14:20 +0530

    added code

--- versions.tf
+++ versions.tf
@@ -16,10 +16,10 @@ provider "azurerm" {
 
  
 
-subscription_id = "b17b0836-3476-4c4b-bae0-309aa4cf65cf"
-  client_id       = "ccbdc08d-fa45-427d-b368-0eee8df3159e"
-  client_secret   = "y-e8Q~HNqfNzaxbtDHZa6cSSvHsXWP9rP0N-Ec36"
-  tenant_id       = "a73f4356-d083-474f-9cb4-5d71a8ac428c"
+subscription_id = 
+  client_id       = 
+  client_secret   = 
+  tenant_id       = 
 
 
 
